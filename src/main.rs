32397a44afc6c4ba0f606c2f6d1edab757c6e0ac
Léo LEESCO <leo.leesco@gmail.com>
2024-02-19 11:50:39 +0100

    remove unnecessary comments

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,6 @@ const ordinal_numbers: [&str; 12] = [
     "Twelve drummers drumming",
 ];
 
-// const lyrics: &str = "On the twelfth day of Christmas,
-// my true love gave to me
-// Twelve drummers drumming,
-// Eleven pipers piping,
-// Ten lords a-leaping,
-// Nine ladies dancing,
-// Eight maids a-milking,
-// Seven swans a-swimming,
-// Six geese a-laying,
-// Five golden rings,
-// Four calling birds,
-// Three French hens,
-// Two turtle doves,
-// And a partridge in a pear tree!";
-
 fn main() {
     for (i, day) in cardinal_numbers.iter().enumerate() {
         println!(
